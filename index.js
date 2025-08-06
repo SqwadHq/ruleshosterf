6581ab06ca2bd0e734ad1cb650f6898a5028799d
ljorgens <leejorgensen22@gmail.com>
2025-08-06 05:48:35 -0700

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from "url";
+const app = express();
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Optionally: explicitly handle "/" if you want to force index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+console.log("App listening on port " + port);
+app.listen(port);
